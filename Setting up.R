5bf5e31570c5bb6b7c25deb9d4272d196f8004c2
Anri Sakakibara <anrisakaki@gmail.com>
2025-06-23 19:48:42 +0200

    Setting up

--- Setting up.R
+++ Setting up.R
@@ -23,6 +23,10 @@ dist_shp <- read_sf("SHP/pak_adm_wfp_20220909_shp/pak_admbnda_adm2_wfp_20220909.
 dist_pk <- read_sf("SHP/UC-shapefile/Adminbdy Shapefile/District_Boundary.shp")
 uc_pk <- read_sf("SHP/UC-shapefile/Adminbdy Shapefile/Union_Council.shp")
 
+flood10_shp <- read_sf("SHP/pak_adm4_2010floodaffected_pco_20150826/Pak_adm4_2010FloodAffected_pco_20150826/Pak_adm4_2010FloodAffected_pco_20150826.shp")
+
+dhs_shp <- read_sf("data/geo_pk1991_2017/geo_pk1991_2017.shp")
+
 # SUCCESS DATA
 succ <- read_sav("data/data/EL-HH Rooster Data Combined V3.sav")
 
@@ -38,3 +42,6 @@ punjab_wm <- read_sav("data/MICS/Pakistan Punjab MICS6 Datasets/Pakistan Punjab 
 
 sindh_hl <- read_sav("data/MICS/Pakistan Sindh MICS6 Datasets/Pakistan Sindh MICS6 SPSS Datasets/hl.sav")
 sindh_wm <- read_sav("data/MICS/Pakistan Sindh MICS6 Datasets/Pakistan Sindh MICS6 SPSS Datasets/wm.sav")
+
+# DHS 
+dhs_17 <- read_sav("data/DHS/PDHS 2017-18/PKIR71SV/PKIR71FL.SAV")
