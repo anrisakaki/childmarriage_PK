7d5d493f92a884446bbe73bab0bb1fd5606335b7
Anri Sakakibara <anrisakaki@gmail.com>
2025-07-24 13:37:50 +0200

    Cleaning DHS

--- Cleaning DHS data.R
+++ Cleaning DHS data.R
@@ -9,9 +9,6 @@ names(dhs_w_06) <- gsub("\\$", "_", names(dhs_w_06))
 names(dhs_w_06) <- gsub("[^a-zA-Z0-9_]", "_", names(dhs_w_06))  
 names(dhs_w_06) <- tolower(names(dhs_w_06))
 
-write_dta(dhs_hh_06, "dhs_hh_06.dta")
-write_dta(dhs_w_06, "dhs_w_06.dta")
-
 # 2017
 names(dhs_w_12) <- make.names(names(dhs_w_12), unique = TRUE)
 names(dhs_w_12) <- gsub("\\$", "_", names(dhs_w_12))  
@@ -23,9 +20,6 @@ names(dhs_hh_12) <- gsub("\\$", "_", names(dhs_hh_12))
 names(dhs_hh_12) <- gsub("[^a-zA-Z0-9_]", "_", names(dhs_hh_12))  
 names(dhs_hh_12) <- tolower(names(dhs_hh_12))
 
-write_dta(dhs_hh_12, "dhs_hh_12.dta")
-write_dta(dhs_w_12, "dhs_w_12.dta")
-
 # 2017
 names(dhs_w_17) <- make.names(names(dhs_w_17), unique = TRUE)
 names(dhs_w_17) <- gsub("\\$", "_", names(dhs_w_17))  
@@ -36,9 +30,6 @@ names(dhs_hh_17) <- make.names(names(dhs_hh_17), unique = TRUE)
 names(dhs_hh_17) <- gsub("\\$", "_", names(dhs_hh_17))  
 names(dhs_hh_17) <- gsub("[^a-zA-Z0-9_]", "_", names(dhs_hh_17))  
 names(dhs_hh_17) <- tolower(names(dhs_hh_17))
-
-write_dta(dhs_hh_17, "dhs_hh_17.dta")
-write_dta(dhs_w_17, "dhs_w_17.dta")
 
 clean_dhs_hh_fn <- function(i){
   i %>% 
@@ -187,6 +178,9 @@ dhs06 <- dhs_w_06 %>%
   select(year, caseid, cluster, regionid, districtid, urban, hh, iv, hhid, ivid, monthint, yrint, birthda, birthmo, birthyr, age, yrschool, educattain, marst, u16, u18,
          marr_fbirth, chborn, agemarr, agefbirth, nchild, work, ind, indgen, husband_age, husband_educ, husband_ind, husband_indgen, husband_agri, agri_land, livestock, wealthind, wt, hhwt)
 
+write_dta(dhs06, "dhs06.dta")
+save(dhs06, file = "dhs06.Rda")
+
 # 2012 
 
 dhs12_hh <- dhs_hh_12 %>% 
@@ -204,6 +198,9 @@ dhs12 <- dhs_w_12 %>%
   select(year, caseid, cluster, regionid, districtid, urban, hh, iv, hhid, ivid, monthint, yrint, birthda, birthmo, birthyr, age, yrschool, educattain, marst, u16, u18,
          marr_fbirth, chborn, agemarr, agefbirth, nchild, work, ind, indgen, dm1, dm2, dm3, dm4, dm_index, dv1, dv2, dv3, dv4, dv5, dv_index,
          husband_age, husband_educ, husband_ind, husband_indgen, husband_agri, agri_land, livestock, wealthind, wt, dv_wt, hhwt)
+
+write_dta(dhs12, "dhs12.dta")
+save(dhs12, file = "dhs12.Rda")
 
 # 2017 
 
@@ -224,103 +221,11 @@ dhs17 <- dhs_w_17 %>%
          ipv1, ipv2, ipv3, ipv4, ipv5, ipv6, ipv7, ipv8, ipv9, ipv10, ipv11, ipv12, ipv13, any_viol, less_viol, severe_viol, sexual_viol,
          husband_age, husband_educ, husband_ind, husband_indgen, husband_agri, agri_land, livestock, wealthind, wt, dv_wt, hhwt)
 
+write_dta(dhs17, "dhs17.dta")
+save(dhs17, file = "dhs17.Rda")
+
 # Combining all
 dhs <- bind_rows(dhs06, dhs12, dhs17)
 
-# Creating SHP 
-dhs$district <- as_factor(dhs$districtid)
-dhs$region <- as_factor(dhs$regionid)
-
-dhs_dist_06_shp <- dhs_dist_06_shp %>%
-  select(DHSCLUST, geometry) %>% 
-  rename(cluster = DHSCLUST) %>% 
-  st_make_valid()
-dhs_dist_17_shp <- dhs_dist_17_shp %>%
-  select(DHSCLUST, geometry) %>% 
-  rename(cluster = DHSCLUST) %>% 
-  st_make_valid()
-dhs_dist_06_shp <- st_transform(dhs_dist_06_shp, st_crs(dist_pk))
-dhs_dist_17_shp <- st_transform(dhs_dist_17_shp, st_crs(dist_pk))
-dist_pk  <- st_make_valid(dist_pk) %>% select(PROVINCE, DISTRICT, geometry)
-dhs_dist_06_shp <- st_join(dhs_dist_06_shp, dist_pk)
-dhs_dist_17_shp <- st_join(dhs_dist_17_shp, dist_pk)
-  
-dhs06_shp <- left_join(dhs06, dhs_dist_06_shp)
-dhs17_shp <- left_join(dhs06, dhs_dist_17_shp)
-
-dhs0617 <- bind_rows(dhs06_shp, dhs17_shp)
-
-dhs0617_sum <- dhs0617 %>% 
-  
-
-dhs_sum_fn <- function(i){
-  i %>% 
-    left_join(dhs_dist) %>% 
-    mutate(district_recode = toupper(as.character(district_recode)),
-           region_recode = toupper(as.character(region_recode)),
-           district_recode = ifelse(district == "Mandi Bahauddin", "MANDI BAHAUDDIN", district_recode),
-           district_recode = case_when(
-             district_recode == "JAFARABAD" ~ "JAFFARABAD",
-             district_recode == "KACHHI (BOLAN)" ~ "KACHHI",
-             district_recode == "KECH (TURBAT)" ~ "KECH",
-             district_recode == "SHERANI" ~ "SHEERANI",
-             district_recode == "NORTH WAZIRISTAN AGENCY" ~ "N. WAZIRASTAN",
-             district_recode == "SOUTH WAZIRISTAN AGENCY" ~ "S. WAZIRASTAN",
-             district_recode == "D. I. KHAN" ~ "D I KHAN",
-             district_recode == "TOR GHAR" ~ "TORDHER",
-             district_recode == "DERA GHAZI KHAN" ~ "D G KHAN",
-             district_recode == "MUZAFFARGARH" ~ "MUZAFARGARH",
-             district_recode == "TOBA TEK SINGH" ~ "T. T SINGH",
-             district_recode == "KAMBAR SHAHDADKOT" ~ "SHAHDAD KOT",
-             district_recode == "KARACHI MALIR" ~ "MALIR",
-             district_recode == "NAUSHAHRO FIROZE" ~ "NAUSHAHRO FEROZ",
-             district_recode == "RAHIM YAR KHAN" ~ "R Y KHAN",
-             district_recode == "SHAHEED BENAZIRABAD" ~ "S. BENAZIRABAD",
-             district_recode == "SHIKARPUR" ~ "SHIKARPHUR",
-             district_recode == "SUJAWAL" ~ "SUJJAWAL",
-             district_recode == "TANDO ALLAHYAR" ~ "T. AYAR",
-             district_recode == "TANDO MUHAMMAD KHAN" ~ "T. M KHAN",
-             TRUE ~ district_recode),
-           region_recode = case_when(
-             region_recode == "GILGIT BALTISTAN" ~ "GB",
-             region_recode == "ICT" ~ "FEDERAL CAPITAL TERRITORY",
-             region_recode == "KPK" ~ "KHYBER PAKHTUNKHWA",
-             TRUE ~ region_recode
-           )) %>% 
-    group_by(region_recode, district_recode) %>% 
-    summarise(
-      u16 = weighted.mean(u16, wt, na.rm = T),
-      u18 = weighted.mean(u18, wt, na.rm = T),
-      agefbirth = weighted.mean(agefbirth, wt, na.rm = T),
-      agemarr = weighted.mean(agemarr, wt),
-      chborn = weighted.mean(chborn, wt),
-      nchild = weighted.mean(nchild, wt),
-      work = weighted.mean(work, wt, na.rm = T),
-      yrschool = weighted.mean(yrschool, wt),
-      less_viol = weighted.mean(less_viol, dv_wt, na.rm = T),
-      severe_viol = weighted.mean(severe_viol, dv_wt, na.rm = T),
-      sexual_viol = weighted.mean(sexual_viol, dv_wt, na.rm = T)
-    ) 
-}
-
-dhs_dist_sum <- dhs17 %>% 
-  dhs_sum_fn()
-
-dhs_dist_2024_sum <- dhs17 %>% 
-  filter(age > 19 & age < 25) %>% 
-  dhs_sum_fn()
-
-dist_pk_shp <- dist_pk %>%
-  rename(region_recode = PROVINCE, district_recode = DISTRICT) %>%
-  select(region_recode, district_recode, geometry) %>% 
-  st_make_valid()
-
-dhs_dist_sum_shp <- dist_pk_shp %>%
-  full_join(dhs_dist_sum, by = c("region_recode", "district_recode")) %>% 
-  filter(!is.na(region_recode))
-
-dhs_dist_2024_sum_shp <- dist_pk_shp %>%
-  full_join(dhs_dist_2024_sum, by = c("region_recode", "district_recode")) %>% 
-  filter(!is.na(region_recode))
-
-
+write_dta(dhs, "dhs061217_clean.dta")
+save(dhs, file = "dhs061217_clean.Rda")
